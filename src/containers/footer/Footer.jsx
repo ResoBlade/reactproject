c072a5849cb94a4750a5d578869299a0c2004404
ResoBlade <jhenlov@gmail.com>
2022-05-04 10:52:55 +0200

    Finished

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <div className="gpt3__footer section__padding">
             <div className="gpt3__footer-heading">
-                <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+                <h1 className="gradient__text">Do you want to step in to the future before others?</h1>
             </div>
             <div className="gpt3__footer-btn">
                 <p>Request Early Access</p>
@@ -34,7 +34,7 @@ const Footer = () => {
                     <h4>Get in touch</h4>
                     <p>Crechterwoord K12 182 DK Alknjkcb</p>
                     <p>085-132567</p>
-                    <p>info@payme.net</p>
+                    <p>who@knows.net</p>
                 </div>
             </div>
 
